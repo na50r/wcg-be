37ded04a30ae8685fe38757eccce1e787569ae92
na50r <shanrosan@gmail.com>
2025-08-01 16:46:17 +0200

    start achievement feature

---
+++ achievements.go
@@ -0,0 +1,8 @@
+package main
+
+// TODO: Figure out a neat way to achievements
+// Requirement: Must be easily extensible
+
+func CheckAchievements(s *APIServer, game *Game, player *Player) error {
+	return nil
+}
